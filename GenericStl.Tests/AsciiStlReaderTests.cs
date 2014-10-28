c078401e814117c9506e87f191b5db2d5b566c9a
Marco Spatz <marco.spatz@gmail.com>
2014-10-28 20:57:27 +0100

    added basics

--- GenericStl.Tests/AsciiStlReaderTests.cs
+++ GenericStl.Tests/AsciiStlReaderTests.cs
@@ -5,16 +5,56 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using FluentAssertions;
+using GenericStl;
+using System.IO;
+using Vertex = System.Tuple<float, float, float>;
+using Normal = System.Tuple<float, float, float>;
+using Triangle = System.Tuple<System.Tuple<float, float, float>, System.Tuple<float, float, float>, System.Tuple<float, float, float>, System.Tuple<float, float, float>>;
 
 namespace GenericStl.Tests
 {
+    public static class TestHelpers
+    {
+        public static Vertex CreateVertex(float x, float y, float z)
+        {
+            return new Vertex(x, y, z);
+        }
+
+        public static Normal CreateNormal(float x, float y, float z)
+        {
+            return new Normal(x, y, z);
+        }
+
+        public static Triangle CreateTriangle(Vertex a, Vertex b, Vertex c, Normal n)
+        {
+            return new Triangle(a, b, c, n);
+        }
+    } 
+
+   
+
     [TestFixture]
     public class AsciiStlReaderTests
     {
-        [Test]
-        public void Test()
-        {
+        private static string asciiTestFile = @".\TestData\ascii_block.stl";
+        private AsciiStlReader<Triangle, Vertex, Normal> _objectUnderTest;
 
+        
+
+        [SetUp]
+        public void SetUp()
+        {
+            this._objectUnderTest = new AsciiStlReader<Triangle, Vertex, Normal>(TestHelpers.CreateTriangle, TestHelpers.CreateVertex, TestHelpers.CreateNormal);
+        }
+
+        [Test]
+        public void Read_ReturnsExpected()
+        {
+            var stlFileContent = File.ReadAllLines(asciiTestFile);
+
+            var result = this._objectUnderTest.Read(stlFileContent);
+
+            result.Should().HaveCount(12);
         }
     }
 }
--- GenericStl/AsciiStlReader.cs
+++ GenericStl/AsciiStlReader.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace GenericStl
 {
-    public class AsciiStlReader
+    public class AsciiStlReader<TTriangle, TVector, TVertex>
     {
+        private readonly Func<float, float, float, TVertex> _createVertex;
+        private readonly Func<float, float, float, TVector> _createNormal;
+        private readonly Func<TVertex, TVertex, TVertex, TVector, TTriangle> _createTriangle;
+
+        public AsciiStlReader(Func<TVertex, TVertex, TVertex, TVector, TTriangle> createTriangle, Func<float, float, float, TVertex> createVertex, Func<float, float, float, TVector> createNormal)
+        {
+
+            this._createTriangle = createTriangle;
+            this._createNormal = createNormal;
+            this._createVertex = createVertex;
+        }
+
+        public TTriangle[] Read(string[] fileContent)
+        {
+            var stlContent = string.Join("\n", fileContent.Select(l => l.Trim())
+                .SkipWhile(l => l.StartsWith("solid", StringComparison.InvariantCultureIgnoreCase))
+                .TakeWhile(l => !l.StartsWith("endsolid", StringComparison.InvariantCultureIgnoreCase))
+                .ToArray() );
+
+            System.Console.WriteLine(stlContent);
+
+            return null;
+                    
+        }
     }
 }

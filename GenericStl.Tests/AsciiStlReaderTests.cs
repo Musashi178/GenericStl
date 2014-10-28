3cb813fa1d9765c6c059f1a78f7d02d3334f01c8
Marco Spatz <marco.spatz@gmail.com>
2014-10-28 20:23:53 +0100

    added basic classes

---
+++ GenericStl.Tests/AsciiStlReaderTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace GenericStl.Tests
+{
+    [TestFixture]
+    public class AsciiStlReaderTests
+    {
+        [Test]
+        public void Test()
+        {
+
+        }
+    }
+}

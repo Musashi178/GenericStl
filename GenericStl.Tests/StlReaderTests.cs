27ad354c3ae576c92986f89db4877e9f559f2252
Marco Spatz <marco.spatz@gmail.com>
2014-10-29 19:06:04 +0100

    added a stl reader that can read ascii and binary

---
+++ GenericStl.Tests/StlReaderTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using GenericStl.Tests.TestDataStructures;
+using NUnit.Framework;
+
+namespace GenericStl.Tests
+{
+    public class StlReaderTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _objectUnderTest = new StlReader<Triangle, Vertex, Normal>(TestHelpers.CreateTriangle, TestHelpers.CreateVertex, TestHelpers.CreateNormal);
+        }
+
+        public IEnumerable StlFiles
+        {
+            get
+            {
+                yield return new TestCaseData(@".\TestData\ascii_block.stl").SetName("Ascii file");
+                yield return new TestCaseData(@".\TestData\binary_block.stl").SetName("Binary file");
+            }
+        } 
+        private StlReader<Triangle, Vertex, Normal> _objectUnderTest;
+
+        [Test]
+        [TestCaseSource("StlFiles")]
+        public void ReadFromFile_ReturnsExpectedTriangles(string fileName)
+        {
+            var result = _objectUnderTest.ReadFromFile(fileName);
+
+            result.Should().BeEquivalentTo(TestHelpers.BlockExpectedResult);
+        }
+
+    }
+}
---
+++ GenericStl/StlReader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace GenericStl
+{
+    public class StlReader<TTriangle, TVertex, TNormal> : StlReaderBase<TTriangle, TVertex, TNormal>
+    {
+        public StlReader(Func<TVertex, TVertex, TVertex, TNormal, TTriangle> createTriangle, Func<float, float, float, TVertex> createVertex, Func<float, float, float, TNormal> createNormal) : base(createTriangle, createVertex, createNormal)
+        {
+        }
+
+        public override IEnumerable<TTriangle> ReadFromFile(string fileName)
+        {
+            using (var fs = File.OpenRead(fileName))
+            {
+                foreach (var triangle in ReadFromStream(fs))
+                {
+                    yield return triangle;
+                }
+            }
+        }
+
+        public override IEnumerable<TTriangle> ReadFromStream(Stream s)
+        {
+            if (IsBinaryStl(s))
+            {
+                return new BinaryStlReader<TTriangle, TVertex, TNormal>(CreateTriangle, CreateVertex, CreateNormal).ReadFromStream(s);
+            }
+
+            return new AsciiStlReader<TTriangle, TVertex, TNormal>(CreateTriangle, CreateVertex, CreateNormal).ReadFromStream(s);
+        }
+
+        private bool IsBinaryStl(Stream stream)
+        {
+            try
+            {
+                using (var r = new StreamReader(stream, Encoding.UTF8, true, 1024, true))
+                {
+                    var buf = new char[20];
+                    r.ReadBlock(buf, 0, 20);
+                    var start = new string(buf);
+                    return !start.TrimStart().StartsWith("solid", StringComparison.OrdinalIgnoreCase);
+                }
+            }
+            finally
+            {
+                stream.Seek(0, SeekOrigin.Begin);
+            }
+        }
+    }
+}

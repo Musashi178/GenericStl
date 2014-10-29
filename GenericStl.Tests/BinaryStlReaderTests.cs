a360d404de41ca2485db080d4cdf35b7ec7b25f2
Marco Spatz <marco.spatz@gmail.com>
2014-10-29 11:10:02 +0100

    implemented binary reader

---
+++ GenericStl.Tests/BinaryStlReaderTests.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+using Vertex = System.Tuple<float, float, float>;
+using Normal = System.Tuple<float, float, float>;
+using Triangle = System.Tuple<System.Tuple<float, float, float>, System.Tuple<float, float, float>, System.Tuple<float, float, float>, System.Tuple<float, float, float>>;
+
+namespace GenericStl.Tests
+{
+    [TestFixture]
+    public class BinaryStlReaderTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _objectUnderTest = new BinaryStlReader<Triangle, Vertex, Normal>(TestHelpers.CreateTriangle, TestHelpers.CreateVertex, TestHelpers.CreateNormal);
+        }
+
+        private BinaryStlReader<Triangle, Vertex, Vertex> _objectUnderTest;
+        private const string BinaryTestFile = @".\TestData\binary_block.stl";
+
+        [Test]
+        public void ReadFile_WithBinaryBlockStl_ReturnsExpectedTriangles()
+        {
+            var result = _objectUnderTest.ReadFile(BinaryTestFile);
+
+            result.Should().BeEquivalentTo(TestHelpers.BlockExpectedResult);
+        }
+
+        [Test]
+        public void Read_WithBinaryBlockStl_ReturnsExpectedTriangles()
+        {
+            var stlFileContent = File.ReadAllBytes(BinaryTestFile);
+
+            var result = _objectUnderTest.Read(stlFileContent);
+
+            result.Should().BeEquivalentTo(TestHelpers.BlockExpectedResult);
+        }
+    }
+}
--- GenericStl/BinaryStlReader.cs
+++ GenericStl/BinaryStlReader.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 
 namespace GenericStl
 {
@@ -12,7 +14,59 @@ namespace GenericStl
 
         public override IEnumerable<TTriangle> ReadFile(string fileName)
         {
-            return null;
+            using (var fs = File.OpenRead(fileName))
+            {
+                foreach (var triangle in Read(fs)) yield return triangle;
+            }
+        }
+
+        private IEnumerable<TTriangle> Read(Stream s)
+        {
+            using (var reader = new BinaryReader(s))
+            {
+                reader.ReadBytes(80); //header
+
+                var numTriangles = reader.ReadInt32();
+
+                for (int i = 0; i < numTriangles; ++i)
+                {
+                    yield return ReadTriangle(reader);
+                }
+
+                Debug.Assert(s.Position == s.Length);
+            }
+        }
+
+
+        private TTriangle ReadTriangle(BinaryReader reader)
+        {
+            Debug.Assert(reader != null);
+
+            var n = ReadNormal(reader);
+            var v1 = ReadVertex(reader);
+            var v2 = ReadVertex(reader);
+            var v3 = ReadVertex(reader);
+            reader.ReadBytes(2); // attribute byte count
+
+            return CreateTriangle(v1, v2, v3, n);
+        }
+
+        private TVertex ReadVertex(BinaryReader reader)
+        {
+            return CreateVertex(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle());
+        }
+
+        private TVector ReadNormal(BinaryReader reader)
+        {
+            return CreateNormal(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle());
+        }
+
+        public IEnumerable<TTriangle> Read(byte[] data)
+        {
+            using (var s = new MemoryStream(data, false))
+            {
+                foreach (var triangle in Read(s)) yield return triangle;
+            }    
         }
     }
 }

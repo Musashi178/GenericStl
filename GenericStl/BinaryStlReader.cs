bcce73d98df6312ede98a96f2524b519123113ec
Marco Spatz <marco.spatz@gmail.com>
2014-10-29 10:33:19 +0100

    extracted StlReaderBase class

---
+++ GenericStl/BinaryStlReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericStl
+{
+    public class BinaryStlReader<TTriangle, TVector, TVertex> : StlReaderBase<TTriangle, TVector, TVertex>
+    {
+        public BinaryStlReader(Func<TVertex, TVertex, TVertex, TVector, TTriangle> createTriangle, Func<float, float, float, TVertex> createVertex, Func<float, float, float, TVector> createNormal) : base(createTriangle, createVertex, createNormal)
+        {
+
+        }
+
+        public override IEnumerable<TTriangle> ReadFile(string fileName)
+        {
+            return null;
+        }
+    }
+}
---
+++ GenericStl/StlReaderBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericStl
+{
+    public abstract class StlReaderBase<TTriangle, TVector, TVertex> : IStlReader<TTriangle>
+    {
+        protected readonly Func<float, float, float, TVector> CreateNormal;
+        protected readonly Func<TVertex, TVertex, TVertex, TVector, TTriangle> CreateTriangle;
+        protected readonly Func<float, float, float, TVertex> CreateVertex;
+
+        protected StlReaderBase(Func<TVertex, TVertex, TVertex, TVector, TTriangle> createTriangle, Func<float, float, float, TVertex> createVertex, Func<float, float, float, TVector> createNormal)
+        {
+            if (createTriangle == null)
+            {
+                throw new ArgumentNullException("createTriangle");
+            }
+            if (createVertex == null)
+            {
+                throw new ArgumentNullException("createVertex");
+            }
+            if (createNormal == null)
+            {
+                throw new ArgumentNullException("createNormal");
+            }
+
+            CreateTriangle = createTriangle;
+            CreateNormal = createNormal;
+            CreateVertex = createVertex;
+        }
+
+        public abstract IEnumerable<TTriangle> ReadFile(string fileName);
+    }
+}
